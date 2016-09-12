8fe5a6a950ecd03e844adf90a6442d05fb57860a
Andi Sugandi <andisugandi@gmail.com>
2016-09-12 10:15:39 +0700

    Add ID translation file

---
+++ trans_id.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="id_ID">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="main.cpp" line="10"/>
+        <source>That&apos;s life.</source>
+        <translation>Inilah hidup.</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="22"/>
+        <source>Hello, my friend!</source>
+        <translation>Halo, temanku!</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="23"/>
+        <source>Goodbye!</source>
+        <translation>Selamat tinggal!</translation>
+    </message>
+</context>
+</TS>
